40f659a4219eb3e2339576f99cfcd4198ccf3ce3
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2022-11-05 15:27:24 +0530

    Add files via upload

---
+++ prbl4.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+*******************************************************************************/
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>ans;
+       
+        for(auto i:s){
+    
+            if(i=='(' || i=='['|| i=='{' ){
+               ans.push(i);
+            }
+           else {
+              if (ans.empty() || (ans.top()=='(' && i!=')') ||(ans.top()=='{' && i!='}') ||(ans.top()=='[' && i!=']'))return false;
+                  ans.pop();
+           }
+            
+        }
+        return ans.empty();
+    }
+};
